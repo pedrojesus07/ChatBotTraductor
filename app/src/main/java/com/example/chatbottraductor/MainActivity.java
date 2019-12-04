cf92b258c77475cf8abbb4577601af166003ebb0
pedrojesus07 <pedrojesus_95_baza@hotmail.com>
2019-12-04 10:30:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chatbottraductor/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.chatbottraductor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.chatbottraductor.api.ChatterBot;
+import com.example.chatbottraductor.api.ChatterBotFactory;
+import com.example.chatbottraductor.api.ChatterBotSession;
+import com.example.chatbottraductor.api.ChatterBotType;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button btSend;
+    private EditText etTexto;
+    private ScrollView svScroll;
+    private TextView tvTexto;
+
+    private ChatterBot bot;
+    private ChatterBotFactory factory;
+    private ChatterBotSession botSession;
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        init();
+        //traducir("");
+    }
+
+
+
+    private void init() {
+        btSend = findViewById(R.id.btSend);
+        etTexto = findViewById(R.id.etTexto);
+        svScroll = findViewById(R.id.svScroll);
+        tvTexto = findViewById(R.id.tvTexto);
+        if(startBot()) {
+            setEvents();
+        }
+    }
+
+    private void chat(final String text) {
+        String response;
+        try {
+            response = getString(R.string.bot) + " " + botSession.think(text);
+        } catch (final Exception e) {
+            response = getString(R.string.exception) + " " + e.toString();
+        }
+        tvTexto.post(showMessage(response));
+    }
+
+    private void setEvents() {
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String text = getString(R.string.you) + " " + etTexto.getText().toString().trim();
+                btSend.setEnabled(false);
+                etTexto.setText("");
+                tvTexto.append(text + "\n");
+                new Thread(){
+                    @Override
+                    public void run() {
+                        chat(text);
+                        //traducir(text);
+                    }
+                }.start();
+            }
+        });
+    }
+
+    private boolean startBot() {
+        boolean result = true;
+        String initialMessage;
+        factory = new ChatterBotFactory();
+        try {
+            bot = factory.create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
+            botSession = bot.createSession();
+            initialMessage = getString(R.string.messageConnected) + "\n";
+        } catch(Exception e) {
+            initialMessage = getString(R.string.messageException) + "\n" + getString(R.string.exception) + " " + e.toString();
+            result = false;
+        }
+        tvTexto.setText(initialMessage);
+        return result;
+    }
+
+    private Runnable showMessage(final String message) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                etTexto.requestFocus();
+                tvTexto.append(message + "\n");
+                svScroll.fullScroll(View.FOCUS_DOWN);
+                btSend.setEnabled(true);
+                hideKeyboard();
+            }
+        };
+    }
+
+    public void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view = getCurrentFocus();
+        if (view == null) {
+            view = new View(this);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    private void traducir(String texto){
+
+        FirebaseTranslatorOptions options =
+                new FirebaseTranslatorOptions.Builder()
+                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+                        .setTargetLanguage(FirebaseTranslateLanguage.ES)
+                        .build();
+
+        final FirebaseTranslator englishGermanTranslator= FirebaseNaturalLanguage.getInstance().getTranslator(options);
+
+
+        new Thread(){
+            @Override
+            public void run() {
+                FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                        .requireWifi()
+                        .build();
+                englishGermanTranslator.downloadModelIfNeeded(conditions)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void v) {
+                                        // Model downloaded successfully. Okay to start translating.
+                                        // (Set a flag, unhide the translation UI, etc.)
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Model couldn’t be downloaded or other internal error.
+                                        // ...
+                                    }
+                                });
+            }
+        }.run();
+
+
+
+        englishGermanTranslator.translate("hello")
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@NonNull String translatedText) {
+                                // Translation successful.
+                                Log.v("xxx",translatedText);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Error.
+                                // ...
+                            }
+                        });
+
+
+    }
+}
